9d67094a81f4cf11e24115b3e15934c983694f10
Jordi Lucas <jordi@joyjet.com>
2018-02-20 17:03:32 -0300

    Add HERE map the fragment

--- app/src/main/java/com/jordilucas/heremap/MainActivity.java
+++ app/src/main/java/com/jordilucas/heremap/MainActivity.java
@@ -3,7 +3,12 @@ package com.jordilucas.heremap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.here.android.mpa.mapping.Map;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private Map map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
